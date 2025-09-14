8a7a39bc3bbf8162dba7573ad550baf48acb06ae
sangyeons57 <sangyeons57@gmail.com>
2025-09-14 15:57:21 +0900

    설정 추가

---
+++ tcp/src/Main.java
@@ -0,0 +1,10 @@
+// Minimal Java entry point for future TCP server work
+// Folder root: teamnova-omok-server/tcp
+// Usage (PowerShell, from tcp folder):
+//   javac .\src\Main.java
+//   java -cp .\src Main
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
