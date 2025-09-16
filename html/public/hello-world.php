d897f0b4cd8e497dd38b0ff8ea8b7363c2e44e76
sangyeons57 <sangyeons57@gmail.com>
2025-09-16 23:27:44 +0900

    [php] 회원 가입 추가

--- html/public/hello-world.php
+++ html/public/hello-world.php
@@ -1,6 +1,7 @@
 <?php
 // hello.php
 header('Content-Type: text/plain; charset=utf-8');
+opcache_reset();
 echo "hello world \n";
 echo "SAPI=".php_sapi_name()."\n";
 var_dump(getenv('DB_HOST'), getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'));
