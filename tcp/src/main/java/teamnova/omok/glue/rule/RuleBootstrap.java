205606f015f1aff80effdfeda8420ed266a253b2
sangyeons57 <sangyeons57@gmail.com>
2025-10-17 13:49:57 +0900

    [java] 매칭 널널하게

--- tcp/src/main/java/teamnova/omok/glue/rule/RuleBootstrap.java
+++ tcp/src/main/java/teamnova/omok/glue/rule/RuleBootstrap.java
@@ -12,10 +12,13 @@ public class RuleBootstrap {
 
     public void registerDefaults(RuleRegistry ruleRegistry) {
         Objects.requireNonNull(ruleRegistry, "ruleRegistry");
+        /**
+         * rule 규칙 에서 문제가 너무 많이 발생해 잠시 주석 처리
         ruleRegistry.register(new EveryFiveTurnBlockerRule());
         ruleRegistry.register(new PerTurnAdjacentBlockerRule());
         ruleRegistry.register(new EveryTwoTurnJokerRule());
         ruleRegistry.register(new GoCaptureRule());
         ruleRegistry.register(new RotatePlayerStonesRule());
+         **/
     }
 }
